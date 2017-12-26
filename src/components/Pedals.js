25fc65c8910706beeda8b107909b18e77419ae4d
Aaron Drobek <adrobek09@gmail.com>
2017-12-26 14:55:35 -0500

    filter search feature added

--- src/components/Pedals.js
+++ src/components/Pedals.js
@@ -6,11 +6,25 @@ import products from '../data/products';
 export default class Pedals extends Component{
   constructor(props){
     super(props);
+    this.updateSearch=this.updateSearch.bind(this);
+    this.state={
+      data: [],
+      search: '',
+      pedals: []
+    }
   }
+
+  updateSearch(event){
+  this.setState({search: event.target.value.substr(0, 20)});
+}
+
   render(){
+    let filterPedals = products.filter((product) =>{
+      return product.Brand.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    })
     console.log(products, 'this is products list');
 
-    let listProducts = products.map((product, i)=>{
+    let listProducts = filterPedals.map((product, i)=>{
       // console.log(product.Brand, 'this is Brand Name*************');
       return(
         <div className = 'each_pedal' key={i}>
@@ -21,7 +35,7 @@ export default class Pedals extends Component{
     return(
       <div className='pedals_box' >
         <div className = 'pedals_box_content'>
-          <input className = 'search_box' placeholder = 'search by brand, model number, or pedal name' />
+          <input className = 'search_box' onChange={this.updateSearch} value={this.state.search} placeholder = 'search by brand, model number, or pedal name' />
 
           {listProducts}
 
