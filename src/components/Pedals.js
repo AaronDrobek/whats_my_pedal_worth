c8548ae4e917051e9a254fe7905c9d085ab36664
Aaron Drobek <adrobek09@gmail.com>
2017-12-26 19:34:45 -0500

    bugs, just bugs

--- src/components/Pedals.js
+++ src/components/Pedals.js
@@ -21,21 +21,29 @@ export default class Pedals extends Component{
   render(){
 
     let filterPedals = products.filter((product) =>{
-
+            // console.log(product, 'product at the point of filter>>>>>>>>>>>>>>>>>>>');
+            // return product.Brand; product.productNAME;
         return product.Brand.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        // return 'product';{
+        //   'Brand';
+        //   'productNAME';
+        //   'productID';
+        // }
 
     })
-    console.log(products, 'this is products list');
+
+    // console.log(products, 'this is products list');
     let listProducts;
-    if (filterPedals.length > 1){
+    if (filterPedals.length > 0){
     listProducts = filterPedals.map((product, i)=>{
       // console.log(product.Brand, 'this is Brand Name*************')
       // console.log(product.productNAME, 'this is product Name@@@@@@@@@@');
-      console.log(product.highPrice, 'this is high price');
+      // console.log(product.highPrice, 'this is high price');
 
       return(
         <div className = 'each_pedal' key={i}>
           <h3 style={{textAlign: 'center'}}>{product.Brand}</h3>
+          <h4 style={{textAlign: 'center'}}>{product.productNAME}</h4>
         </div>
       )
     })
